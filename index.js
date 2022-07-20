8ff89786214613ab5b0d1a9c3c47f91e65dc0083
Ronica Singh <ladyinblack2010rs@gmail.com>
2022-07-20 13:41:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,15 +29,3 @@ console.log('-10ºC = ' + convertCtoF(-10) + 'ºF');
 console.log('0ºC = ' + convertCtoF(0) + 'ºF');
 console.log('20ºC = ' + convertCtoF(20) + 'ºF');
 console.log('30ºC = ' + convertCtoF(30) + 'ºF');
-
-/** PROBLEM EXPLANATION:
- * The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
- * You are given a variable 'celsius' representing a temperature in Celsius.  Use the variable 'fahrenheit' already defined and apply the algorithm to assign it the corresponding temperature in Fahrenheit.
- */
-
-/** HINT:
- * Keep in mind the 'order of operation' check the link in the links section for more information.
- * Relevant Links:
- *    - The Order of Operations: PEMDAS (http://www.purplemath.com/modules/orderops.htm)
- *    - Order of Operation: Video (https://www.khanacademy.org/math/pre-algebra/order-of-operations/order_of_operations/v/order-of-operations)
- */
